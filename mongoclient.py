82d22c91aee6751510c0dea3a229f82f8098a332
Jeffrey Klassen <jeff.klassen@siginttech.com>
2017-01-22 16:59:21 +0100

    basic mongo client

---
+++ mongoclient.py
@@ -0,0 +1,28 @@
+"""Handle CRUD actions to/from MongoDB """
+import json
+from pymongo import MongoClient
+
+def get_DB():
+    """Connect to mongoDB """
+    client = MongoClient('localhost', 27017)
+    return client.strava
+
+def get_activity_collection(db):
+    return db.activity
+
+def insert_activity(activity):
+    collection = get_activity_collection( get_DB())
+    insertion = collection.insert_one(activity)
+    print insertion
+
+activity = {"test": 15
+    }
+
+def list_activities():
+    collection = get_activity_collection( get_DB())
+    for activity in collection.find():
+        print(activity)
+
+#insert_activity(activity)
+
+list_activities()
