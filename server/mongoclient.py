ba1e7f1707d733ade27333950fa70ff1833bc236
Jeffrey Klassen <jeff.klassen@siginttech.com>
2017-02-05 17:37:01 +0100

    remove extraneous functions

--- server/mongoclient.py
+++ server/mongoclient.py
@@ -14,11 +14,6 @@ def insert_activity(activity):
     collection = get_activity_collection(get_DB())
     insertion = collection.insert_one(activity)
 
-def list_activities():
-    collection = get_activity_collection(get_DB())
-    for activity in collection.find():
-        print(activity)
-
 def list_athlete_activities(athleteID):
     collection = get_activity_collection(get_DB())
     for activity in collection.find({"athlete.id": {"$eq" : athleteID}}):
