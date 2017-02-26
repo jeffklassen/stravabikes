fb0bac8190f54a8e2ed9db473e5eb4114dc6a534
Rose Klassen <rklass08@gmail.com>
2017-02-26 09:01:26 +0100

    Added strava logo

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Header = ({vendorName}) => {
   
     return (<div className="row">
-        <h1 > My {vendorName} Bikes</h1>
-        </div>
+        <div className="col-md-9">
+        <h1 > My {vendorName} Bikes</h1></div>
+        <div className="col-md-3"><img src = './Images/stravaLogo.png' className = "pull-right"></img>
+        </div></div>
     );
 };
 export default Header;
