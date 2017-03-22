b13f85c45a6e32fc6788939cdbbc1159de0afdb9
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-03-22 08:44:09 +0100

    basic router implemented, need to tie everything back together

--- client/app/main.jsx
+++ client/app/main.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header  from './components/Header.jsx';
-import AppSurface  from './components/AppSurface.jsx';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import BikeInfoSurface from './components/bikeinfo/BikeInfoSurface.jsx';
+import LoginSurface from './components/login/LoginSurface.jsx';
+import * as request from 'superagent';
 
-const App = () => {
-    return (
-        <div className="container">
-            <Header/>
-            <AppSurface />
-        </div>
-    );
-};
+class AppSurface extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { ready: false };
+        this.isAuthenticated = this.isAuthenticated.bind(this);
+        this.isAuthenticated();
+    }
+    isAuthenticated() {
+        request.get('api/isAuthenticated')
+            .then(() => {
+                window.history.pushState('', '', '');
+                this.setState({ ready: true, isLoggedIn: true });
+            })
+            .catch((err) => {
+                console.log('isAuthenticated', err);
+                this.setState({ ready: true, isLoggedIn: false });
+            });
+    }
+    render() {
+        return this.state.ready && this.state.isLoggedIn ?
+            (<BikeInfoSurface />) :
+            (<LoginSurface isAuthenticated={this.isAuthenticated} />);
+
+    }
+}
+
+
 
 
 ReactDOM.render(
-    React.createElement(App),
+    <Router history={Router.hashHistory}>
+        <div className="container">
+            <Header />
+            <Route exact={true} path="/" component={AppSurface} />
+            <Route exact={true} path="/login" component={LoginSurface} />
+        </div>
+    </Router>,
     document.getElementById('react-app')
 );
