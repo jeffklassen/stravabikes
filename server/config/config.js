737d1a1267071fd5326302a5da3c97c868a92dc4
Jeffrey Klassen <jeffrey.s.klassen@gmail.com>
2017-10-13 07:59:22 +0200

    update config to accept a separate strava client config when in dev

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,24 @@
 const path = require('path');
 
 let config = {};
-config.options = { root: path.normalize(__dirname + '/../../client/') };
+config.options = {
+    root: path.normalize(__dirname + '/../../client/')
+};
 
 config.strava = {
-    authProvider : {
+    authProvider: {
         authUrl: 'https://www.strava.com/oauth/authorize',
-        clientId: 5893,
-        redirectUri: process.env.NODE_ENV == 'dev'?'http://localhost:3000/login': 'https://trackmybike.herokuapp.com/login'
-    
+        clientId: process.env.NODE_ENV == 'dev' ? 20781 : 5893,
+        redirectUri: process.env.NODE_ENV == 'dev' ? 'http://localhost:3000/login' : 'https://trackmybike.herokuapp.com/login'
+
     },
-    clientSecret : process.env.CLIENTSECRET
+    clientSecret: process.env.CLIENTSECRET
 };
 
 
 
 config.mongo = {
-    url : process.env.NODE_ENV == 'dev' ? 'mongodb://localhost/strava' : 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@ds157624.mlab.com:57624/strava'
+    url: process.env.NODE_ENV == 'dev' ? 'mongodb://localhost/strava' : 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@ds157624.mlab.com:57624/strava'
 };
 
 console.log(config.mongo.url);
